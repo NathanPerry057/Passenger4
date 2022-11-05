a6844294ed056309dfbae695eaecd8c325c744ef
NathanPerry057 <G00394355@atu.ie>
2022-11-05 15:08:09 +0000

    Created a titlecheck in the passenger method and a test with assertEquals for success and failure

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -1,4 +1,29 @@
 package ie.atu;
 
 public class Passenger {
+    private String title;
+    private String name;
+    private int id;
+    private int phone;
+    private int age;
+    public static String titleCheck(String title) {
+        if(title.contains("MR"))
+        {
+            return title ;
+
+        }
+        else if (title.contains("MRS"))
+        {
+            return title;
+        }
+        else if (title.contains("MS"))
+        {
+            return title;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+
+    }
 }
--- src/test/java/ie/atu/PassengerTest.java
+++ src/test/java/ie/atu/PassengerTest.java
@@ -2,16 +2,35 @@ package ie.atu;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PassengerTest {
+public class PassengerTest {
 
     @BeforeEach
     void setUp() {
+
     }
+
+    @Test
+    public void title() {
+        assertEquals("MR", Passenger.titleCheck("MR"));
+        assertEquals("MRS", Passenger.titleCheck("Mster"));
+    }
+
 
     @AfterEach
     void tearDown() {
     }
+
+
+    public String getTitle(String title) {
+
+        return title;
+    }
+
+
 }
+
+//Use getters and setters in the test class and pass through the values to the main constructor
